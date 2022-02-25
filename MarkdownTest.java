6f27319f8aff679df277217da88ffc8d1737f25b
KidaChh029 <97996620+KidaChh029@users.noreply.github.com>
2022-02-25 15:00:18 -0800

    Update MarkdownTest.java

--- MarkdownTest.java
+++ MarkdownTest.java
@@ -93,7 +93,26 @@ public class MarkdownTest {
         List<String> expect = List.of("https://something.com");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }*/
+    
+        @Test
+    public void testSnippet1() throws IOException {
+        String contents= Files.readString(Path.of("./snippet1.md"));
+        List<String> expect = List.of();
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
 
-    
-    
+    @Test
+    public void testSnippet2() throws IOException {
+        String contents= Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents= Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+   
 }
